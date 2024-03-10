5173c43f99c75e3b80acf099f62b361af8916f50
Grena30 <grenadinu@yahoo.com>
2024-03-10 18:54:41 +0200

    feat: add lab1

---
+++ lab1/main.py
@@ -0,0 +1,80 @@
+from utils import *
+
+
+# TASK 1
+Ts1 = 0.01
+Ts2 = 0.001
+limit_signal = 1
+limit_filter = 50
+ts1_plot = round(limit_signal/Ts1)
+ts2_plot = round(limit_signal/Ts2)
+
+# 1.1
+t1, x1 = generate_white_noise(Ts1, limit_filter)
+plot_signal(t1[:ts1_plot], x1[:ts1_plot], title="White noise, step 0.01")
+
+# 1.2
+plot_signal(t1[:ts1_plot], x1[:ts1_plot], title="White noise histogram, step 0.01", histogram=True)
+
+# 1.3
+t2, x2 = generate_white_noise(Ts2, limit_filter)
+plot_signal(t2[:ts2_plot], x2[:ts2_plot], title="White noise, step 0.001")
+
+# 1.4
+plot_signal(t2[:ts2_plot], x2[:ts2_plot], title="White noise histogram, step 0.001", histogram=True)
+
+# 1.5
+y1 = filter_signal(Ts1, x1)
+plot_signal(t1, y1, title="Filtered noise, step 0.01")
+
+# 1.6
+y2 = filter_signal(Ts2, x2)
+plot_signal(t2, y2, title="Filtered noise, step 0.001")
+
+
+# TASK 2
+R1 = 50
+R2 = 1
+step1 = 1
+step2 = 0.001
+M = [3, 5, 10, 20, 50, 100]
+
+# 2.1
+m, s = generate_signal(R1, step1)
+plot_multiple_signals(m, [s], title="Original signal")
+
+# 2.2
+m, d = generate_signal(R1, step1, random=True)
+plot_multiple_signals(m, [d], title="Noise signal")
+
+# # 2.3
+plot_multiple_signals(m, [s, d], labels=["Original signal",  "Noise signal"], title="Original and noise signal")
+
+# 2.4
+x = s + d
+plot_multiple_signals(m, [s, x], labels=["Original signal", "Modified signal"], title="Original and modified signal")
+
+# 2.5
+y1 = filter_signal_mfa(M[0], x)
+plot_multiple_signals(m, [y1, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=3")
+
+# 2.6
+y2 = filter_signal_mfa(M[1], x)
+plot_multiple_signals(m, [y2, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=5")
+
+y3 = filter_signal_mfa(M[2], x)
+plot_multiple_signals(m, [y3, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=10")
+
+# 2.7
+m, s = generate_signal(R2, step2, sawtooth_signal=True)
+m, d = generate_signal(R2, step2, random=True)
+x = s + d
+y4 = filter_signal_mfa(M[3], s)
+plot_multiple_signals(m, [y4, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=20")
+
+# 2.8
+y5 = filter_signal_mfa(M[4], s)
+plot_multiple_signals(m, [y5, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=50")
+
+y6 = filter_signal_mfa(M[5], s)
+plot_multiple_signals(m, [y6, x, s], labels=["Filtered signal", "Modified signal", "Original signal"], title="Multiple signals, M=100")
---
+++ lab1/utils.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import lfilter, sawtooth
+
+image_counter = 1
+
+def generate_white_noise(Ts, range):
+    t = np.arange(0, range + Ts, Ts)
+    x = np.random.randn(len(t))
+    
+    return t, x
+    
+    
+def plot_signal(t, x, title=None, histogram=False):
+    global image_counter
+    if histogram:
+        plt.hist(x, bins=50, color='skyblue', edgecolor='black')
+        plt.xlabel('Value')
+        plt.ylabel('Frequency')
+        plt.title(title if title else 'Histogram of white noise')
+        plt.grid(axis='y', alpha=0.75)
+        plt.savefig(f'./plots/{image_counter}. {title}.png')
+        image_counter += 1
+        plt.show()
+        return
+    else:
+        plt.plot(t, x)
+        plt.grid()
+        plt.xlabel('Time(s)')
+        plt.ylabel('Function y(t)')
+        plt.title(title if title else 'White noise signal')
+        plt.savefig(f'./plots/{image_counter}. {title}.png')
+        image_counter += 1
+        plt.show()
+        return
+
+
+def filter_signal(Ts, x):
+    om0 = 2 * np.pi
+    dz = 0.005
+    A = 1
+    oms = om0 * Ts
+    a = [1 + 2 * dz * oms + oms ** 2, -2 * (1 + dz * oms), 1]
+    b = [A * 2 * oms ** 2]
+    y1 = lfilter(b, a, x) 
+    
+    return y1
+
+
+def generate_signal(R, step,sawtooth_signal=False, random=False):
+    m = np.arange(0, R, step)
+    if random:
+        d = np.random.rand(len(m)) - 0.5
+        
+        return m, d
+    
+    if sawtooth_signal:
+        s = 2 * sawtooth(3 * np.pi * m + np.pi/6)
+        return m, s
+    
+    s = 2 * m * (0.9 ** m)
+    
+    return m, s
+
+
+def plot_multiple_signals(m, signals, labels=None, title=None):
+    global image_counter
+    if not labels:
+        labels = [f'Signal {i+1}' for i in range(len(signals))]
+        
+    for i in range(len(signals)):
+        plt.plot(m, signals[i], label=labels[i])
+    
+    plt.grid()
+    plt.xlabel('Time index n')
+    plt.ylabel('Amplitude')
+    plt.title(title if title else 'Multiple signals')
+    
+    if len(signals) > 1:
+        plt.legend()
+        
+    plt.savefig(f'./plots/{image_counter}. {title}.png')
+    image_counter += 1
+    plt.show()
+
+
+def filter_signal_mfa(M, x):
+    a = 1
+    b = np.ones(M) / M
+    y1 = lfilter(b, a, x) 
+    
+    return y1
